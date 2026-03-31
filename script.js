710ea11479e256827b602c4d9983c6dfb9de3ab5
chakali shanmukha sai <shanmukhasai@chakalis-MacBook-Pro.local>
2026-04-01 00:23:55 +0530

    milestone-2-progress

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ if (e.key === "Enter") {
 searchMovies();
 }
 });
+async function searchMovies() {
+
+const query = searchInput.value.trim();
+
+if (!query) return;
+
+showLoading();
+
+try {
+
+const response = await fetch(
+`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+);
+
+const data = await response.json();
+
+hideLoading();
+
+if (data.Response === "False") {
+showEmptyState("No movies found");
+return;
+}
+
+displayMovies(data.Search);
+
+} catch (error) {
+
+hideLoading();
+showEmptyState("Something went wrong");
+
+}
+
+}
