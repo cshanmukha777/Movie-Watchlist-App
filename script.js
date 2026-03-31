6f1775fc649dba31c7aca3ddc1c03918410e1712
chakali shanmukha sai <shanmukhasai@chakalis-MacBook-Pro.local>
2026-04-01 00:13:32 +0530

    milestone-2-progress

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+const moviesContainer = document.getElementById("movies");
+const loading = document.getElementById("loading");
