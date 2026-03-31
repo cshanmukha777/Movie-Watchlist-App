393e1c80f36945ddf26a03e2be16acf0f06d7f50
chakali shanmukha sai <shanmukhasai@chakalis-MacBook-Pro.local>
2026-04-01 00:22:19 +0530

    milestone-2-progress

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 const moviesContainer = document.getElementById("movies");
 const loading = document.getElementById("loading");
-const API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=7a53f7e7";
+
 
 searchBtn.addEventListener("click", searchMovies);
 
