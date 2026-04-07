6f82f0294dd42710e455d2b9ed022c93f52fdb07
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-07 21:13:46 +0530

    Variables & Setup

--- script.js
+++ script.js
@@ -2,82 +2,12 @@ const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 const moviesContainer = document.getElementById("movies");
 const loading = document.getElementById("loading");
-const API_KEY = "7a53f7e7"
 
-searchBtn.addEventListener("click", searchMovies);
+const filterInput = document.getElementById("keywordFilter");
+const sortSelect = document.getElementById("sortBy");
 
-searchInput.addEventListener("keypress", function(e) {
-if (e.key === "Enter") {
-searchMovies();
-}
-});
-async function searchMovies() {
+const API_KEY = "7a53f7e7";
 
-const query = searchInput.value.trim();
-
-if (!query) return;
-
-showLoading();
-
-try {
-
-const response = await fetch(
-`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
-);
-
-const data = await response.json();
-
-hideLoading();
-
-if (data.Response === "False") {
-showEmptyState("No movies found");
-return;
-}
-
-displayMovies(data.Search);
-
-} catch (error) {
-
-hideLoading();
-showEmptyState("Something went wrong");
-
-}
-
-}
-function displayMovies(movies) {
-
-moviesContainer.innerHTML = movies
-.map(movie => `
-
-<div class="movie-card">
-<img 
-src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300x450"}" 
-alt="${movie.Title}"
-/>
-
-<div class="movie-info">
-<h3>${movie.Title}</h3>
-<p>${movie.Year}</p>
-</div>
-
-</div>
-
-`).join("");
-
-}
-function showLoading() {
-loading.classList.remove("hidden");
-moviesContainer.innerHTML = "";
-}
-
-function hideLoading() {
-loading.classList.add("hidden");
-}
-
-function showEmptyState(message) {
-moviesContainer.innerHTML = `
-<div class="empty">
-${message}
-</div>
-`;
-}
+let moviesData = [];
+let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
