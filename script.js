3d056917d0f6314907930ae7930dcb9b4e4cbce8
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-07 21:15:02 +0530

    Filter + Sort + Display

--- script.js
+++ script.js
@@ -56,3 +56,74 @@ showEmptyState("Something went wrong");
 }
 
 }
+function renderMovies() {
+
+const keyword = filterInput.value.toLowerCase();
+const sortType = sortSelect.value;
+
+let filteredMovies = moviesData
+.filter(movie =>
+movie.Title.toLowerCase().includes(keyword)
+)
+
+.sort((a,b)=>{
+
+if(sortType === "title-asc"){
+return a.Title.localeCompare(b.Title)
+}
+
+if(sortType === "title-desc"){
+return b.Title.localeCompare(a.Title)
+}
+
+if(sortType === "year-asc"){
+return a.Year - b.Year
+}
+
+if(sortType === "year-desc"){
+return b.Year - a.Year
+}
+
+return 0;
+
+});
+
+displayMovies(filteredMovies);
+
+}
+
+function displayMovies(movies) {
+
+moviesContainer.innerHTML = movies
+.map(movie => `
+
+<div class="movie-card">
+
+<img 
+src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300x450"}" 
+alt="${movie.Title}"
+/>
+
+<div class="movie-info">
+<h3>${movie.Title}</h3>
+<p>${movie.Year}</p>
+
+<div class="movie-actions">
+
+<button onclick="addWatchlist('${movie.imdbID}')">
+Watchlist
+</button>
+
+<button onclick="addFavorite('${movie.imdbID}')">
+❤ Favorite
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`).join("");
+
+}
