01709bf4eca43ba16024ea3f9099a0f6b10645d3
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-07 21:14:30 +0530

    Search Movies API

--- script.js
+++ script.js
@@ -21,3 +21,38 @@ searchMovies();
 
 filterInput.addEventListener("input", renderMovies);
 sortSelect.addEventListener("change", renderMovies);
+async function searchMovies() {
+
+const query = searchInput.value.trim();
+
+if (!query) return;
+
+showLoading();
+
+try {
+
+const response = await fetch(
+`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+);
+
+const data = await response.json();
+
+hideLoading();
+
+if (data.Response === "False") {
+showEmptyState("No movies found");
+return;
+}
+
+moviesData = data.Search;
+
+renderMovies();
+
+} catch (error) {
+
+hideLoading();
+showEmptyState("Something went wrong");
+
+}
+
+}
